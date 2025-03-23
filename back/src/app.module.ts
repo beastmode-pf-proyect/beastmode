50c5c064bb20caaf19f779c9ae3d748974000d6d
beastmode-pf-proyect <pfproyecthenry@gmail.com>
2025-03-23 11:26:55 -0500

    Conexion a base de datos

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,7 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import typeOrm from './config/typeorm';
+
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    load: [typeOrm],
+  }),
+  TypeOrmModule.forRootAsync({
+    inject: [ConfigService],
+    // eslint-disable-next-line @typescript-eslint/require-await
+    useFactory: async (config: ConfigService): Promise<TypeOrmModuleOptions> => {
+      const typeOrmConfig = config.get<TypeOrmModuleOptions>('typeorm');
+      if (!typeOrmConfig) {
+        throw new Error('TypeORM configuration is not defined in the environment.');
+      }
+      return typeOrmConfig;
+    },
+  }),
+  
+  ],
   controllers: [],
   providers: [],
 })
