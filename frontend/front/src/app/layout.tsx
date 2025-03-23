1036aec594564bcb91392bf410f09b42ecba93b1
rene-orozco <oroozco.rene@gmail.com>
2025-03-22 20:11:26 -0500

    agregando linea de codigo para que el header y footer siempre ocupen su espacio

--- frontend/front/src/app/layout.tsx
+++ frontend/front/src/app/layout.tsx
@@ -25,9 +25,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <div style={{ flex: 1 }}>{children}</div>
       </body>
     </html>
   );
