d8ee7a322f68a5df453c6b9c4f74811c9165f078
rene-orozco <oroozco.rene@gmail.com>
2025-03-26 18:47:17 -0500

    modificando la vista header dando un margin y un bg

--- frontend/front/src/Views/Header/Header.tsx
+++ frontend/front/src/Views/Header/Header.tsx
@@ -1,12 +1,12 @@
 import Navbar from "@/Components/Navbar";
 import React, { ReactElement } from "react";
 
-export const Header:React.FC = ():ReactElement => {
+export const Header: React.FC = (): ReactElement => {
 
-    return(
-        <>
+    return (
+        <div className="mt-1 bg-red-900 bg-opacity-50">
             <Navbar />
-        </>
+        </div>
     )
 }
 
