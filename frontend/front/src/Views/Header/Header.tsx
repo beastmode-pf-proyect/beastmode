42100b9c45dbb06231259869d61009e590c6a11d
rene-orozco <oroozco.rene@gmail.com>
2025-03-26 18:20:31 -0500

    creacion de la vista header

---
+++ frontend/front/src/Views/Header/Header.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/Components/Navbar";
+import React, { ReactElement } from "react";
+
+export const Header:React.FC = ():ReactElement => {
+
+    return(
+        <>
+            <Navbar />
+        </>
+    )
+}
+
+export default Header;
