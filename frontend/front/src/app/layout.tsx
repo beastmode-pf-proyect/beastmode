442246c09c36127fc862fe94b7862c045f4414fd
rene-orozco <oroozco.rene@gmail.com>
2025-03-26 18:21:50 -0500

    posicionando la vista header en el layout, para que siempre este visible

--- frontend/front/src/app/layout.tsx
+++ frontend/front/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/Components/Footer/Footer";
+import Header from "@/Views/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}>
+        <Header />
         <div style={{ flex: 1 }}>{children}</div>
         <Footer />
       </body>
