b7518bfb3e93499646a8375a9f2dfe462dfe4235
rene-orozco <oroozco.rene@gmail.com>
2025-03-26 18:43:26 -0500

    modificando los elementos del navbar

--- frontend/front/src/Components/Navbar/Navbar.tsx
+++ frontend/front/src/Components/Navbar/Navbar.tsx
@@ -1,10 +1,27 @@
 import React, { ReactElement } from "react";
 
-export const Navbar:React.FC = ():ReactElement => {
+// importamos el menu harcodeado
+import { itemNavbar } from "./itemNavbar";
 
-    return(
+// importamos Link para hacer el Router
+import Link from "next/link";
+
+
+
+export const Navbar: React.FC = (): ReactElement => {
+
+    return (
         <>
-        
+            <div className="flex flex-row flex-wrap justify-center gap-2 w-full">
+                {itemNavbar.map((elemento, index) => (
+                    <Link href={elemento.href} key={index}>
+                        /* From Uiverse.io by JesusRafaelNavaCruz */
+                        <button className="cursor-pointer uppercase bg-white px-4 py-2 active:translate-x-0.5 active:translate-y-0.5 hover:shadow-[0.5rem_0.5rem_#F44336,-0.5rem_-0.5rem_#00BCD4] transition">
+                            {elemento.label}
+                        </button>
+                    </Link>
+                ))}
+            </div>
         </>
     )
 }
