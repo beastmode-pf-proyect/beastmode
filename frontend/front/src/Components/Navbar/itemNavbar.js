8c3e271b6cd49fb3dbd876929c7dfc86371f2710
rene-orozco <oroozco.rene@gmail.com>
2025-03-26 18:26:46 -0500

    agregando menu harcodeo para el navbar

---
+++ frontend/front/src/Components/Navbar/itemNavbar.js
@@ -0,0 +1,10 @@
+const itemNavbar = [
+    {href:"/", label: "Home"},
+    {href:"/About", label: "About Us"},
+    {href:"/Subscriptions", label: "Subscriptions"},
+    // {href:"/Landing", label: "Landing"}
+]
+
+export {
+    itemNavbar
+}
