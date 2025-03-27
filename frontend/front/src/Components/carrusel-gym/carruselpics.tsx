ea98bdf28657b6ca4ce5d59f9d5e051038f6b730
AndreaVPalacios <andreita.estudia@gmail.com>
2025-03-27 08:54:12 -0400

    Solucionando_images_gympics

--- frontend/front/src/Components/carrusel-gym/carruselpics.tsx
+++ frontend/front/src/Components/carrusel-gym/carruselpics.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect, useRef } from "react";
-import Image from "next/image";
+import Image from "next/legacy/image";
 
 const GymCarousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
