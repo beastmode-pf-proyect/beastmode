712860f428f0534ee7e44c1063b30168bf434f7b
rene-orozco <oroozco.rene@gmail.com>
2025-03-26 19:08:39 -0500

    ahora si dejando el home en blanco

--- frontend/front/src/app/page.tsx
+++ frontend/front/src/app/page.tsx
@@ -1,11 +1,4 @@
-"use client";
-
-import Button from "@/Components/Button/button";
-
 export default function Home() {
-  const handleClick = () => {
-    window.alert("Hiciste click!");
-  };
 
   return (
     <>
