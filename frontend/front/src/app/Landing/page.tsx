7b59aa61ee36c2a6c6e1559b792c3374918df3f8
AndreaVPalacios <andreita.estudia@gmail.com>
2025-03-29 18:34:56 -0400

    Landing_Page_Perfeccionada

--- frontend/front/src/app/Landing/page.tsx
+++ frontend/front/src/app/Landing/page.tsx
@@ -7,18 +7,26 @@ import TestimonialsSection from "@/Components/testimonies/testimonials";
 const Landing = () => {
   return (
     <div>
-      <div>
+      <div className="relative h-screen w-full">
+        {/* Imagen para móviles */}
         <div
+          className="absolute inset-0 bg-cover bg-center md:hidden"
+          style={{ backgroundImage: "url('/images/landing-phone.png')" }}
+        ></div>
+        {/* Imagen para tablets */}
+        <div
+          className="absolute inset-0 bg-cover bg-center hidden md:block lg:hidden"
+          style={{ backgroundImage: "url('/images/landing_res.png')" }}
+        ></div>
+
+        {/* Imagen para desktop */}
+        <div
+          className="absolute inset-0 bg-cover bg-center hidden lg:block"
           style={{
-            backgroundImage: "url('/images/landingpage.png')",
-            backgroundSize: "cover",
-            width: "screen",
-            position: "relative",
-            height: "100vh",
+            backgroundImage: "url('/images/landingpage1.png')",
           }}
         ></div>
       </div>
-
       <AboutUs />
 
       <GymCarousel />
