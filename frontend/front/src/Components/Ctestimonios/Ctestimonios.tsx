dcc5ed211682b9802d7f4f2dd630cbcf18d2a080
rene-orozco <oroozco.rene@gmail.com>
2025-03-29 15:46:38 -0500

    agregando cositas en css

--- frontend/front/src/Components/Ctestimonios/Ctestimonios.tsx
+++ frontend/front/src/Components/Ctestimonios/Ctestimonios.tsx
@@ -25,23 +25,25 @@ export const Ctestimonios: React.FC = (): ReactElement => {
 
     return (
         <>
-            <div className="ml-5 mr-5 mt-5">
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 pt-10 pb-26">
+            <div className="ml-5 mr-5 mt-14 flex justify-center items-center">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-20 pt-6 pb-16">
                     {testimoniosAleatorios.length > 0 ? (
                         testimoniosAleatorios.map((elemento, index) => (
                             <div
                                 key={index}
-                                className="bg-white shadow-lg rounded-xl p-4 flex flex-col items-center text-center">
+                                className="bg-gray-100 shadow-md rounded-lg p-3 flex flex-col items-center text-center w-44 
+                                    transition-all duration-300 transform hover:scale-105 hover:bg-gray-200"
+                            >
                                 <Image
                                     src={elemento.imagen}
                                     alt={`Imagen de ${elemento.nombre}`}
-                                    width={96}
-                                    height={96}
-                                    className="w-24 h-24 rounded-full object-cover mb-3"
+                                    width={64}
+                                    height={64}
+                                    className="w-16 h-16 rounded-full object-cover mb-2"
                                 />
-                                <h3 className="text-lg font-semibold">{elemento.nombre}</h3>
-                                <p className="text-gray-600">{elemento.descripcion}</p>
-                                <p className="mt-2 text-yellow-500">⭐ {elemento.calificacion}</p>
+                                <h3 className="text-sm font-semibold">{elemento.nombre}</h3>
+                                <p className="text-xs text-gray-600">{elemento.descripcion}</p>
+                                <p className="mt-1 text-yellow-500 text-sm">⭐ {elemento.calificacion}</p>
                             </div>
                         ))
                     ) : (
