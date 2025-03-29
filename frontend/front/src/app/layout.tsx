0c9c1c526cd7a8c7de6cdb39d9b6d2534ae5143f
rene-orozco <oroozco.rene@gmail.com>
2025-03-29 12:49:16 -0500

    acomodando el header y footer para que esten en su lugar

--- frontend/front/src/app/layout.tsx
+++ frontend/front/src/app/layout.tsx
@@ -15,10 +15,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" className="h-full">
+      <body className="h-full flex flex-col">
         <Header />
-        <div>{children}</div>
+        <main className="flex-1">{children}</main>
         <Footer />
       </body>
     </html>
